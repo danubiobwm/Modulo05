55bb130d98863e04015469689a00e4f6cd54749f
danubiobwm <danubio.bwm@gmail.com>
2019-09-01 17:12:59 -0300

    Estilos globais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { Title } from './styles';
 
 function Main() {
-  return (
-    <Title error={false}>
-      Main
-      <small>Menor</small>
-    </Title>
-  );
+  return <Title>Hello world</Title>;
 }
 
 export default Main;
